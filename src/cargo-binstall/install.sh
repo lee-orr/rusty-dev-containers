6407487badfe8e845069a1f6fa46e8587b7a5ed9
lee-orr <lee-orr@users.noreply.github.com>
2023-05-03 21:12:45 +0000

    binstall, expand and watch

--- src/cargo-binstall/install.sh
+++ src/cargo-binstall/install.sh
@@ -2,9 +2,11 @@
 set -e
 
 if ! which rustup > /dev/null; then
-    which curl > /dev/null || (apt update && apt install curl build-essential -y -qq)
+    which curl > /dev/null || (apt update && apt install curl -y -qq)
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
     source $HOME/.cargo/env
 fi
 
-cargo install cargo-binstall
+dpkg -l | grep build-essential || (apt update && apt install build-essential -y -qq)
+
+cargo install cargo-binstall --locked
--- src/cargo-expand/install.sh
+++ src/cargo-expand/install.sh
@@ -2,11 +2,15 @@
 set -e
 
 if ! which rustup > /dev/null; then
-    which curl > /dev/null || (apt update && apt install curl build-essential -y -qq)
+    which curl > /dev/null || (apt update && apt install curl -y -qq)
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
     source $HOME/.cargo/env
 fi
 
+dpkg -l | grep build-essential || (apt update && apt install build-essential -y -qq)
 
+if ! cargo install --list | grep "cargo-binstall" > /dev/null; then
+    cargo install cargo-binstall
+fi
 
-cargo install cargo-expand
+cargo bininstall cargo-expand
