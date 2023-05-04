b5ce0da8e74165b7895702828f8027141c035140
lee-orr <lee-orr@users.noreply.github.com>
2023-05-04 01:48:00 +0000

    spin, wasmcloud, helix

---
+++ src/fermyon-spin/install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+if ! which rustup > /dev/null; then
+    which curl > /dev/null || (apt update && apt install curl -y -qq)
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+    source $HOME/.cargo/env
+fi
+
+dpkg -l | grep build-essential || (apt update && apt install build-essential -y -qq)
+dpkg -l | grep libssl-dev || (apt update && apt install libssl-dev -y -qq)
+dpkg -l | grep pkg-config || (apt update && apt install pkg-config -y -qq)
+dpkg -l | grep "ii  git" || (apt update && apt install git -y -qq)
+
+
+rustup target add wasm32-unknown-unknown
+rustup target add wasm32-wasi
+
+git clone https://github.com/fermyon/spin
+cd ./spin
+cargo install --locked --path ./
+
+spin plugins install --url https://github.com/itowlson/spin-pluginify/releases/download/canary/pluginify.json --yes
---
+++ src/helix/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+if ! which rustup > /dev/null; then
+    which curl > /dev/null || (apt update && apt install curl -y -qq)
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+    source $HOME/.cargo/env
+fi
+
+dpkg -l | grep build-essential || (apt update && apt install build-essential -y -qq)
+dpkg -l | grep pkg-config || (apt update && apt install pkg-config -y -qq)
+dpkg -l | grep "ii  git" || (apt update && apt install git -y -qq)
+
+
+git clone https://github.com/helix-editor/helix
+cd helix
+cargo install --path helix-term --locked
