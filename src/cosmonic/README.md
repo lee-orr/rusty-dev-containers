9e92a3640960faa4cb0aba3a8bfbec4a9b3ff9f4
lee-orr <lee-orr@users.noreply.github.com>
2023-05-12 13:28:12 +0000

    add cosmonic

---
+++ src/cosmonic/README.md
@@ -0,0 +1,24 @@
+
+# Wasmcloud (wasmcloud)
+
+A feature to install Wasmcloud
+
+## Example Usage
+
+```json
+"features": {
+    "ghcr.io/lee-orr/rusty-dev-containers/wasmcloud:0": {}
+}
+```
+
+## Options
+
+| Options Id | Description | Type | Default Value |
+|-----|-----|-----|-----|
+
+
+
+
+---
+
+_Note: This file was auto-generated from the [devcontainer-feature.json](https://github.com/lee-orr/rusty-dev-containers/blob/main/src/wasmcloud/devcontainer-feature.json).  Add additional notes to a `NOTES.md`._
---
+++ src/cosmonic/install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+if ! which rustup > /dev/null; then
+    which curl > /dev/null || (apt update && apt install curl -y -qq)
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+    source $HOME/.cargo/env
+fi
+
+dpkg -l | grep build-essential || (apt update && apt install build-essential -y -qq)
+dpkg -l | grep pkg-config || (apt update && apt install pkg-config -y -qq)
+dpkg -l | grep libz-dev|| (apt update && apt install libz-dev -y -qq)
+dpkg -l | grep "ii  git" || (apt update && apt install git -y -qq)
+
+curl -fLO https://www.openssl.org/source/openssl-1.1.1s.tar.gz
+tar -xvf openssl-1.1.1s.tar.gz 
+cd openssl-1.1.1s
+./config --prefix=/usr/local/ssl --openssldir=/usr/local/ssl shared zlib
+make
+make test
+make install
+
+bash -c "$(curl -fsSL https://cosmonic.sh/install.sh)"
+
