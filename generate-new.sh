a1e3ca7076e9c97b26975be7a169666ad74ce834
Lee-Orr <lee-orr@users.noreply.github.com>
2023-05-12 19:20:54 +0000

    fix generation - move templates out of src/test dirs

--- generate-new.sh
+++ generate-new.sh
@@ -2,8 +2,8 @@ echo "Generating Template For $1"
 
 mkdir ./src/$1
 mkdir ./test/$1
-cp -R ./src/template/* ./src/$1/
-cp -R ./test/template/* ./test/$1/
+cp -R ./template/src/* ./src/$1/
+cp -R ./template/test/* ./test/$1/
 
 cd ./src/$1
 grep -RiIl 'feature_id' | xargs sed -i "s/feature_id/$1/g"
