fb11f500226140a80e9e4702c947d9520d7e33aa
lee-orr <lee-orr@users.noreply.github.com>
2023-05-12 16:16:11 +0000

    add ability to generate features from a template

---
+++ generate-new.sh
@@ -0,0 +1,14 @@
+echo "Generating Template For $1"
+
+mkdir ./src/$1
+mkdir ./test/$1
+cp -R ./src/template/* ./src/$1/
+cp -R ./test/template/* ./test/$1/
+
+cd ./src/$1
+grep -RiIl 'feature_id' | xargs sed -i "s/feature_id/$1/g"
+grep -RiIl 'lowercase_title' | xargs sed -i "s/lowercase_title/${2,,}/g"
+grep -RiIl 'upercase_title' | xargs sed -i "s/upercase_title/$2/g"
+
+cd ../../test/$1
+grep -RiIl 'feature_id' | xargs sed -i "s/feature_id/$1/g"
