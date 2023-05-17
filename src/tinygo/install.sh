2ec38cfed407b5fe378b3d0a7e185bccffb3a026
lee-orr <lee-orr@users.noreply.github.com>
2023-05-17 19:11:17 +0000

    update tinygo install

--- src/tinygo/install.sh
+++ src/tinygo/install.sh
@@ -4,12 +4,15 @@ set -e
 echo "Activating feature 'Tiny Go'"
 
 # Tiny Go version 
-VERSION=${VERSION:-"latest"}
+VERSION=${VERSION:-"0.27.0"}
 ARCHITECTURE="amd64"
 
 if [ "$(dpkg --print-architecture)" ==  "arm64" ]; then
     ARCHITECTURE="arm64"
 fi
+
+apt update
+apt install -y curl
 
 LOCALFILE="/tmp/tinygo_${VERSION}_${ARCHITECTURE}.deb"
 TINYURL="https://github.com/tinygo-org/tinygo/releases/download/v${VERSION}/tinygo_${VERSION}_${ARCHITECTURE}.deb"
