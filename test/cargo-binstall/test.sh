75ad4be80c6de61d17bf045bf933dd0eeb410cd2
lee-orr <lee-orr@users.noreply.github.com>
2023-08-06 13:19:58 +0000

    cargo binstall test
    Signed-off-by: lee-orr <lee-orr@users.noreply.github.com>

--- test/cargo-binstall/test.sh
+++ test/cargo-binstall/test.sh
@@ -8,5 +8,9 @@ source dev-container-features-test-lib
 # Definition specific tests
 check "installed" cargo install --list | grep "cargo-binstall"
 
+cargo binstall --locked mprocs -y
+
+check "installed" cargo install --list  | grep "mprocs"
+
 # Report result
 reportResults
