19420b202d6da8df74ef8da3d5d949175ebfe55f
lee-orr <lee-orr@users.noreply.github.com>
2023-08-06 13:12:44 +0000

    add tests for cargo audit
    Signed-off-by: lee-orr <lee-orr@users.noreply.github.com>

--- test/cargo-audit/test.sh
+++ test/cargo-audit/test.sh
@@ -8,5 +8,10 @@ source dev-container-features-test-lib
 # Definition specific tests
 check "installed" cargo install --list | grep "cargo-audit"
 
+cargo new test_pkg
+cd ./test_pkg
+cargo build
+cargo audit
+
 # Report result
 reportResults
